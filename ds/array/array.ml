66ea5c17a9bca7f229fefa0f52759acdb609e4ca
augustin64 <augustin.lucas64@gmail.com>
2021-10-27 18:36:09 +0200

    Ajout de commentaires

--- ds/array/array.ml
+++ ds/array/array.ml
@@ -22,12 +22,14 @@ let minimum t =
     done;
     !m;;
     
+(** [list_of_array t] returns the list corresponding to the array t *)
 let list_of_array t = 
-    let rec aux i = (* transforme t.(0), ..., t.(i) en liste *)
-        if i = -1 then []
-        else t.(i)::aux (i - 1) in
-    aux (Array.length t - 1);;
+    let rec aux i =
+        if i = (Array.length t) then []
+        else t.(i)::aux (i + 1) in
+    aux 0;;
     
+(** [croissant t] returns wether t is increasing or not *)
 let croissant t =
     let res = ref true in
     for i=0 to Array.length t - 2 do
@@ -36,31 +38,18 @@ let croissant t =
     done;
     !res;;
     
+(** [max_local t] finds and returns a local maximum *)
 let max_local t = 
     let n = Array.length t in
-    let rec aux i j = (* cherche un max local entre les indices i et j de t *)
+    let rec aux i j =
         let m = (i + j)/2 in
         if (m = 0 || t.(m) >= t.(m-1)) && (m = n - 1 || t.(m) >= t.(m+1))
-        then m (* m est un max local *)
+        then m
         else if t.(m) < t.(m - 1) then aux i (m - 1)
         else aux (m + 1) j in
     aux 0 (n + 1);;
-
-let tri_comptage t =
-    let m = maximum t in
-    let compte = Array.make (m+1) 0 in
-    let n = Array.length t in
-    for i=0 to n - 1 do
-        compte.(t.(i)) <- compte.(t.(i)) + 1
-    done;
-    let k = ref 0 in (* k est la position où on va ajouter le prochain élément dans t *)
-    for i=0 to m do
-        for j=1 to compte.(i) do
-            t.(!k) <- i;
-            incr k
-        done
-    done;;
     
+(** [tranche_max t] returns the maximum value that can be reached by consecutive items in the array t *)
 let tranche_max t =
     let m = ref t.(0) in
     let m_cur = ref t.(0) in
@@ -70,12 +59,13 @@ let tranche_max t =
     done;
     !m;;
     
+(** [inv t] returns the number of inversions in the array t O(n²) *)
 let inv t =
     let res = ref 0 in
     let n = Array.length t in
-    for i=0 to n - 1 do  (* répété n fois *)
-        for j=i+1 to n - 1 do (* répété au plus n fois *)
-            if t.(i) > t.(j) then res := !res + 1 (* répété au plus n² fois, donc complexité O(n²) *)
+    for i=0 to n - 1 do 
+        for j=i+1 to n - 1 do
+            if t.(i) > t.(j) then res := !res + 1
         done
     done;
     !res;;
