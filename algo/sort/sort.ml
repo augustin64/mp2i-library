1f6e760e26ac8f98fc0f37c4b6c11a7a43815fc9
augustin64 <augustin.lucas64@gmail.com>
2021-10-27 18:35:28 +0200

    Ajout du tri_comptage

--- algo/sort/sort.ml
+++ algo/sort/sort.ml
@@ -14,6 +14,8 @@ let sort_bubble t =
       done
   done;;
 
+
+
 (** [_split l] split l in two equal parts*)
 let rec _split = function
     | [] -> [], []
@@ -28,9 +30,35 @@ let rec _fusion l1 l2 = match l1, l2 with
     | e1::q1, e2::q2 when e1 < e2 -> e1::_fusion q1 l2
     | e1::q1, e2::q2 -> e2::_fusion l1 q2;;
 
-(** [fusion_sort l] sorts list l using fusion sort (O(n???)) *)
+(** [fusion_sort l] sorts list l using fusion sort *)
 let rec fusion_sort = function
     | [] -> []
     | [e] -> [e]
     | l -> let l1, l2 = _split l in
     _fusion (fusion_sort l1) (fusion_sort l2);;
+    
+    
+    
+(** [_maximum t] returns the maximum in an array [t] *)
+let _maximum t = 
+    let m = ref t.(0) in
+    for i=1 to Array.length t - 1 do
+        m := max t.(i) !m
+    done;
+    !m;;
+    
+(** [tri_comptage t] sorts the array t using counting sort *)
+let tri_comptage t =
+    let m = _maximum t in
+    let compte = Array.make (m+1) 0 in
+    let n = Array.length t in
+    for i=0 to n - 1 do
+        compte.(t.(i)) <- compte.(t.(i)) + 1
+    done;
+    let k = ref 0 in
+    for i=0 to m do
+        for j=1 to compte.(i) do
+            t.(!k) <- i;
+            incr k
+        done
+    done;;
